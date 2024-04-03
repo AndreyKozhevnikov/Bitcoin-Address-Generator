ac093c910b31426504bb51a05c3f353f2fd1d4cd
AndreyK <ka1207424@gmail.com>
2024-04-03 20:47:00 +0400

    fix addresses

--- Gen.py
+++ Gen.py
@@ -4,11 +4,37 @@ from Crypto.Hash import SHA256, RIPEMD160
 import base58
 import ecdsa
 from bech32 import bech32_encode, convertbits
+from decimal import *
+import base64
+import math
+import hashlib
+import bech32
+def getbytes(bits):
+    done = False
+    while not done:
+        byte = 0
+        for _ in range(0, 8):
+            try:
+                bit = next(bits)
+            except StopIteration:
+                bit = 0
+                done = True
+            byte = (byte << 1) | bit
+        yield byte
 
 def generate_bitcoin_address():
+
+    # str_of_key = '9a1c78a507689f6f54b847ad1cef1e614ee23f1e'
+    # tt = hashlib.new('sha256', bytes.fromhex(str_of_key)).hexdigest()
+    # bb= hashlib.new('ripemd160', bytes.fromhex(tt)).hexdigest()
     # Generate private key
     private_key = os.urandom(32)
-    fullkey = '80' + private_key.hex()
+    private_key2=72090970136851954874045001246842798852073771335057509966434671426544306327206
+    length = math.ceil(math.log(private_key2, 256))
+    res = int.to_bytes(private_key2, length=length, byteorder='big', signed=False)
+    
+    private_key=res
+    fullkey = '80' + private_key.hex()+'01'
     sha256a = SHA256.new(bytes.fromhex(fullkey)).hexdigest()
     sha256b = SHA256.new(bytes.fromhex(sha256a)).hexdigest()
     WIF = base58.b58encode(bytes.fromhex(fullkey + sha256b[:8]))
@@ -31,15 +57,18 @@ def generate_bitcoin_address():
     checksum = SHA256.new(SHA256.new(bytes.fromhex(public_key_hash)).digest()).hexdigest()[:8]
     p2pkh_address = base58.b58encode(bytes.fromhex(public_key_hash + checksum))
 
-    # Get compressed P2PKH address
+    # Get compressed P2PKH address +
     hash160 = RIPEMD160.new()
     hash160.update(SHA256.new(bytes.fromhex(compressed_public_key)).digest())
-    public_key_hash = '00' + hash160.hexdigest()
+    public_key_hash_clean =  hash160.hexdigest()
+    public_key_hash = '00' + public_key_hash_clean
     checksum = SHA256.new(SHA256.new(bytes.fromhex(public_key_hash)).digest()).hexdigest()[:8]
     compressed_p2pkh_address = base58.b58encode(bytes.fromhex(public_key_hash + checksum))
 
     # Get P2SH address
-    redeem_script = '21' + compressed_public_key + 'ac'
+    #redeem_script = '21' + compressed_public_key + 'ac'
+    redeem_script = '0014' + public_key_hash[2:]
+    p2sh_address2 = base58.b58encode(bytes.fromhex(redeem_script))
     hash160 = RIPEMD160.new()
     hash160.update(SHA256.new(bytes.fromhex(redeem_script)).digest())
     script_hash = '05' + hash160.hexdigest()
@@ -47,21 +76,23 @@ def generate_bitcoin_address():
     p2sh_address = base58.b58encode(bytes.fromhex(script_hash + checksum))
 
     # Get Bech32 address
-    witness_program = bytes([0x00, 0x14]) + hash160.digest()
-    bech32_address = bech32_encode('bc', convertbits(witness_program, 8, 5))
+    witprog = bytes.fromhex(public_key_hash_clean)
+    witver = 0x00
+    hrp = 'bc'
+    bechaddress = bech32.encode(hrp, witver, witprog)
 
     return {
         'private_key': private_key.hex(),
         'WIF': WIF.decode(),
         'public_key': public_key,
         'compressed_public_key': compressed_public_key,
-        'p2pkh_address': p2pkh_address.decode(),
+        #'p2pkh_address': p2pkh_address.decode(),
         'compressed_p2pkh_address': compressed_p2pkh_address.decode(),
         'p2sh_address': p2sh_address.decode(),
-        'bech32_address': bech32_address
+        'bech32_address': bechaddress
     }
 
-num_addresses = int(input('Enter the number of addresses to generate: '))
+num_addresses = 1
 for i in range(num_addresses):
     address_info = generate_bitcoin_address()
     print(f"Address #{i+1}:")
@@ -69,7 +100,7 @@ for i in range(num_addresses):
     print(f"WIF: {address_info['WIF']}")
     print(f"Public Key: {address_info['public_key']}")
     print(f"Compressed Public Key: {address_info['compressed_public_key']}")
-    print(f"P2PKH Address: {address_info['p2pkh_address']}")
+    #print(f"P2PKH Address: {address_info['p2pkh_address']}")
     print(f"Compressed P2PKH Address: {address_info['compressed_p2pkh_address']}")
     print(f"P2SH Address: {address_info['p2sh_address']}")
     print(f"Bech32 Address: {address_info['bech32_address']}\n")
